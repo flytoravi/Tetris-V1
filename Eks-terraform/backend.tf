ca8a9c9547d92cc147d52d80fc0fccd5011e1b54
flytoravi <flytoravi@gmail.com>
2023-12-18 19:52:20 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "handtime-webapp" # Replace with your actual S3 bucket name
+    bucket = "handtime-webapp2" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
